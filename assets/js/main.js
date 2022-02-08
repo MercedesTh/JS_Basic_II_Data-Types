55deb137a599a5a64907f51b1002553a90518262
Mercedes <mercedeswagner84@gmail.com>
2022-02-08 14:28:58 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+// Aufgabe 1 Lev1_4 //
+
+console.log(typeof ("John")); // string
+console.log(typeof (3.14)); // number
+console.log(typeof (NaN)); // number
+console.log(typeof (false)); // boolean
+console.log(typeof ([1, 2, 3, 4])); // object
+console.log(typeof ({ name: 'John', age: 34 })); // object
+console.log(typeof (new Date())); // object
+console.log(typeof (function () { })); // function
+console.log(typeof (null)); //object
+console.log(typeof (" ")); //string
+console.log(typeof (3 + 2 == 5)); // boolean
+console.log(typeof (3 + "3")); // string
+
+
+
+// Aufgabe 2.1 Lev1_8 //
+
+let x = 20;
+let y = 30;
+
+console.log(x + y); // 50
+console.log(x - y); // -10
+console.log(y - x); // 10
+console.log(x * y); // 600
+console.log(x / y); // 0.6666666
+
+let z = 10;
+
+console.log(x * y / z); // 60
+
+let resultOne = 60;
+
+
+
+// Aufgabe 2.2 Lev1_8 //
+
+let a = 15;
+let b = 9;
+
+console.log(a % b); // 6
+
+let c = 20;
+
+console.log((a + b) * c); // 480
+
+let resultTwo = 480;
+
+console.log(a++); // 15???
+console.log(b--); // 9???
+console.log(a - b); // 8
+
+let resultThree = 8
+
+console.log(resultOne % resultTwo); // 60
